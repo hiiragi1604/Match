9434b7bd3084ca4148eae5dcd3779ba315497100
bmQ88rk4Ry?3c?iC <t3doch00@students.oamk.fi>
2025-01-22 13:29:32 +0200

    Skeleton of the client

---
+++ match-client/src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import landingPageStyle from "./LandingPage.module.css"
+
+export const LandingPage = () => {
+  return (
+    <div className={landingPageStyle.container}>
+      
+    </div>
+  )
+}
