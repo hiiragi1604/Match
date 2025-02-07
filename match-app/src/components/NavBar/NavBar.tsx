67da229279da30ab5ba10bc85552599674ebb797
Kyle <tgen107205@gmail.com>
2025-02-07 12:42:14 +0200

    Added basic styling for Navbar and styling elements

--- match-app/src/components/NavBar/NavBar.tsx
+++ match-app/src/components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import navBarStyle from "./NavBar.module.css"
+import navBarStyle from "./NavBar.module.scss"
 import { useAuth } from '../../context/AuthContext';
 import { signOutUser } from '../../utils/signOut';
 
@@ -10,10 +10,9 @@ export const NavBar = () => {
   return (
     <div className={navBarStyle.container}>
         <ul className={navBarStyle.navlist}>
-            <li className={navBarStyle.navitem}><Link to="/logo">Logo</Link></li>
+            <div className={navBarStyle.logo}>Logo </div>
             <li className={navBarStyle.navitem}><Link to="/match">Match</Link></li>
-            <li className={navBarStyle.navitem}><Link to="/community">Community</Link></li>
-            <li className={navBarStyle.navitem}><Link to="/project">Project</Link></li>
+            <li className={navBarStyle.navitem}><Link to="/search">Search</Link></li>
             <li className={navBarStyle.navitem}><Link to="/chat">Chat</Link></li>
             {user ? <button className={navBarStyle.navitem} onClick={signOutUser}>Sign out</button> : <li className={navBarStyle.navitem}><Link to="/signin">Sign in</Link></li>}
             {!user ? <li className={navBarStyle.navitem}><Link to="/register">Register</Link></li> : null}
