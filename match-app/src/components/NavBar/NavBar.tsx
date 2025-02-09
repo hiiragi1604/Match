8b31b6d577d50c35f3802393a4de731aab87fcb1
bmQ88rk4Ry?3c?iC <t3doch00@students.oamk.fi>
2025-02-09 13:01:30 +0200

    added button to change from user mode to owner mode and and reverse

--- match-app/src/components/NavBar/NavBar.tsx
+++ match-app/src/components/NavBar/NavBar.tsx
@@ -6,6 +6,10 @@ import { signOutUser } from '../../utils/signOut';
 
 export const NavBar = () => {
   const { user, loading } = useAuth();
+  const [isOwnerMode, setIsOwnerMode] = React.useState(true);
+  const setOwnerMode = () => {
+    setIsOwnerMode(!isOwnerMode);
+  }
   if (loading) return <div>Loading...</div>;
   return (
     <div className={navBarStyle.container}>
@@ -14,6 +18,7 @@ export const NavBar = () => {
             <li className={navBarStyle.navitem}><Link to="/match">Match</Link></li>
             <li className={navBarStyle.navitem}><Link to="/search">Search</Link></li>
             <li className={navBarStyle.navitem}><Link to="/chat">Chat</Link></li>
+            {isOwnerMode ? <li className={navBarStyle.navitem} onClick={setOwnerMode}>Owner</li> : <li className={navBarStyle.navitem} onClick={setOwnerMode}>User</li>}
             {user ? <button className={navBarStyle.navitem} onClick={signOutUser}>Sign out</button> : <li className={navBarStyle.navitem}><Link to="/signin">Sign in</Link></li>}
             {!user ? <li className={navBarStyle.navitem}><Link to="/register">Register</Link></li> : null}
         </ul>
